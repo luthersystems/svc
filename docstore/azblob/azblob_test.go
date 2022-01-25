c3010e93ce3f75e98758449f9449bdc8a27bde41
Sam Wood <sam.wood@luthersystems.com>
2022-01-25 08:55:44 -0800

    Add cert auth to azblob

--- docstore/azblob/azblob_test.go
+++ docstore/azblob/azblob_test.go
@@ -1,5 +1,4 @@
 // Copyright © 2021 Luther Systems, Ltd. All right reserved.
-
 package azblob
 
 import (
@@ -18,6 +17,9 @@ var (
 )
 
 // TestFunctionalIntegration runs functional tests on azure.
+// export AZ_BLOB_ACCOUNT_NAME="***"
+// export AZ_BLOB_CONTAINER_NAME="***"
+// export AZ_BLOB_ACCOUNT_KEY=$(az storage account keys list --account-name $AZ_BLOB_ACCOUNT_NAME --query '[0].value' | jq -r)
 func TestFunctionalIntegration(t *testing.T) {
 	if !*runIntegration {
 		t.Skip()
@@ -27,17 +29,51 @@ func TestFunctionalIntegration(t *testing.T) {
 	containerName := os.Getenv("AZ_BLOB_CONTAINER_NAME")
 	accountKey := os.Getenv("AZ_BLOB_ACCOUNT_KEY")
 
-	client, err := New("test", accountName, containerName, accountKey)
-	require.NoError(t, err)
-	testKey := fmt.Sprintf("%s-%s", "test", uuid.New().String())
-	data := []byte("test")
-	err = client.Put(testKey, data)
+	store, err := New("test", accountName, containerName, accountKey)
 	require.NoError(t, err)
 
-	b, err := client.Get(testKey)
+	do(t, store)
+}
+
+// TestFunctionalCertificateIntegration runs functional tests on azure.
+// export AZ_BLOB_ACCOUNT_NAME="***"
+// export AZ_BLOB_CONTAINER_NAME="***"
+// export AZ_BLOB_CERT_PATH="***.p12"
+// export AZ_BLOB_CERT_PASSWORD=""
+// export AZ_BLOB_CERT_CLIENT_ID="***"
+// export AZ_BLOB_CERT_TENANT_ID="***"
+func TestFunctionalCertificateIntegration(t *testing.T) {
+	if !*runIntegration {
+		t.Skip()
+	}
+
+	accountName := os.Getenv("AZ_BLOB_ACCOUNT_NAME")
+	containerName := os.Getenv("AZ_BLOB_CONTAINER_NAME")
+	certPath := os.Getenv("AZ_BLOB_CERT_PATH")
+	certPassword := os.Getenv("AZ_BLOB_CERT_PASSWORD")
+	clientID := os.Getenv("AZ_BLOB_CERT_CLIENT_ID")
+	tenantID := os.Getenv("AZ_BLOB_CERT_TENANT_ID")
+
+	store, err := NewFromCertificate("test", accountName, containerName, certPath, certPassword, clientID, tenantID)
+	require.NoError(t, err)
+
+	do(t, store)
+}
+
+func do(t *testing.T, store *Store) {
+	var err error
+	testKey := fmt.Sprintf("%s-%s", "test", uuid.New().String())
+	data := []byte("test")
+	err = store.Put(testKey, data)
+	require.NoError(t, err)
+
+	b, err := store.Get(testKey)
 	require.NoError(t, err)
 	require.Equal(t, b, data)
 
-	_, err = client.Get("fnord-missing")
+	_, err = store.Get("fnord-missing")
 	require.Error(t, err, docstore.ErrRequestNotFound)
+
+	_, err = store.Get("public-009e2eb9-0e36-45b3-9697-f3903f96344f.jpeg")
+	require.NoError(t, err)
 }
