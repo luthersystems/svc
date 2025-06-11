01600dad3749b2bb30194d5115805d8244a42387
sam-at-luther <sam.wood@luthersystems.com>
2025-06-10 17:54:41 -0700

    Add ability to send attachments

--- mailer/mailer.go
+++ mailer/mailer.go
@@ -1,9 +1,13 @@
-// Copyright © 2021 Luther Systems, Ltd. All right reserved.
+// Copyright © 2025 Luther Systems, Ltd. All right reserved.
 
 package mailer
 
 import (
+	"bytes"
 	"context"
+	"fmt"
+	"mime/multipart"
+	"net/textproto"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -66,4 +70,58 @@ func (m *SES) Send(ctx context.Context, content string, email string, subject st
 		return err
 	}
 	return nil
+}
+
+// Attachment represents a file to attach to the email.
+type Attachment struct {
+	Filename string
+	Data     []byte
+}
+
+// SendWithAttachment sends an email with one or more attachments.
+func (m *SES) SendWithAttachment(ctx context.Context, body, to, subject string, attachments []Attachment) error {
+	var buf bytes.Buffer
+	writer := multipart.NewWriter(&buf)
+
+	// Set email headers
+	mimeHeaders := make(map[string]string)
+	mimeHeaders["From"] = m.sender
+	mimeHeaders["To"] = to
+	mimeHeaders["Subject"] = subject
+	mimeHeaders["MIME-Version"] = "1.0"
+	mimeHeaders["Content-Type"] = "multipart/mixed; boundary=" + writer.Boundary()
+
+	// Write email headers
+	var msg bytes.Buffer
+	for k, v := range mimeHeaders {
+		msg.WriteString(fmt.Sprintf("%s: %s\r\n", k, v))
+	}
+	msg.WriteString("\r\n")
+
+	// Write HTML body part
+	bodyWriter, _ := writer.CreatePart(textproto.MIMEHeader{
+		"Content-Type": {"text/html; charset=utf-8"},
+	})
+	bodyWriter.Write([]byte(body))
+
+	// Attach files
+	for _, att := range attachments {
+		partHeader := textproto.MIMEHeader{}
+		partHeader.Set("Content-Type", "application/zip")
+		partHeader.Set("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"`, att.Filename))
+		part, _ := writer.CreatePart(partHeader)
+		part.Write(att.Data)
+	}
+
+	writer.Close()
+	msg.Write(buf.Bytes())
+
+	input := &ses.SendRawEmailInput{
+		RawMessage: &ses.RawMessage{
+			Data: msg.Bytes(),
+		},
+	}
+
+	_, err := m.svc.SendRawEmailWithContext(ctx, input)
+	return err
 }
